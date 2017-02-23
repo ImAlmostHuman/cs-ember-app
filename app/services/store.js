2a2c3b683cdb4f006955aed35da02e474bff810b
tom <thomas.ulman@gmail.com>
2017-02-23 22:15:54 +1100

    tutorial does not seem compatible with current version of ember, leaving it here andm oving on to another tutorial matching verion 2.11, maybe I will return..

--- app/services/store.js
+++ app/services/store.js
@@ -1,24 +1,36 @@
 import Ember from 'ember';
+import LineItem from 'woodland/models/line-item';
+import Order from 'woodland/models/order';
+import Product from 'woodland/models/product';
 
-// export default Ember.Service.extend({
-// 	getOrders(){
-// 		return [
-// 			{ id:'1', name:'Don'},
-// 			{ id:'2', name:'Uschi'}
-// 		];
-// 	}
-// });
+const products = [
+	Product.create({title: 'Tent', price: 10}),
+	Product.create({title: 'Sleeping Bag', price: 5}),
+	Product.create({title: 'Flashlight', price: 3}),
+	Product.create({title: 'First-Aid', price: 2})
+];
+
+const orders = [
+    Order.create({
+        id: '123', name: 'Dusty Coat',
+        items:[
+            LineItems.create({product: products[0], quantity: 1}),
+            LineItems.create({product: products[1], quantity: 1}),
+            LineItems.create({product: products[2], quantity: 0})
+        ]    
+    }),
+    Order.create({
+        id: '124', name: 'Usch Monster',
+        items:[
+            LineItems.create({product: products[0], quantity: 0}),
+            LineItems.create({product: products[1], quantity: 2}),
+            LineItems.create({product: products[2], quantity: 0})
+        ]       
+    })
+];
 
 export default Ember.Service.extend({
-	getOrderById(id) {
-		const orders = this.getOrders();
-		return orders.findBy('id', id);
-	},
-
-	getOrders(){
-		return [
-			{ id:'1', name:'Don'},
-			{ id:'2', name:'Uschi'}
-		];
-	}
+  getOrderById(id) { return orders.findBy('id', id); },
+  getOrders() { return orders; },
+  getProducts() { return products; }
 });
